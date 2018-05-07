8e1f5696cf1c60de1298d1a109e315852e37e2c0
Alex Chernevskiy <lexachsar@gmail.com>
2018-05-08 02:08:43 +0400

    Добавил графики зависимости м.о. числа требований в системе и м.о. длительности пребывания требований в системе от lambda_0.

--- Task1.py
+++ Task1.py
@@ -3,6 +3,9 @@
 
 import numpy
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 # Количество систем обслуживания
 L = 6
 
@@ -21,43 +24,61 @@ TETA = numpy.array([[0, 0.2, 0, 0.8, 0, 0, 0],
 # Вектор интенсивностей обслуживания
 mu = numpy.array( [4, 1, 2, 5, 3, 2, 3] )
 
+Mn = []
+Mu = []
+lambdas0 = []
+
+# Шаг смены lambda_0
+step = 0.1
 # Интенсивность потока из источника
-lambda0 = 0.8
+for lambda0 in numpy.arange(0, 1, step):
 
-# Создаем единичную матрицу, необходимую для решения системы
-I = numpy.eye(L + 1, dtype=float)
+    lambdas0.append(lambda0)
 
-A = TETA.transpose() - I
+    # Создаем единичную матрицу, необходимую для решения системы
+    I = numpy.eye(L + 1, dtype=float)
 
-# Заполняем последнюю строчку матрицы A нулями
-A[L, :] = numpy.ones(L + 1, dtype=float)
+    A = TETA.transpose() - I
 
-# Создаем матрицу-столбец B системы, состоящую из нулей с единицей в нижней строке
-B = numpy.zeros(L + 1, dtype=float)
-B[L] = 1
+    # Заполняем последнюю строчку матрицы A нулями
+    A[L, :] = numpy.ones(L + 1, dtype=float)
 
-# Решаем систему, получаем распределение вероятностей поступления требований в системы
-omega = numpy.linalg.solve(A, B)
+    # Создаем матрицу-столбец B системы, состоящую из нулей с единицей в нижней строке
+    B = numpy.zeros(L + 1, dtype=float)
+    B[L] = 1
 
-# Создаем матрицу интенсивностей потоков
-lambdai = numpy.zeros(L + 1)
-lambdai[0] = lambda0
+    # Решаем систему, получаем распределение вероятностей поступления требований в системы
+    omega = numpy.linalg.solve(A, B)
 
-# Рассчет интенсивностей потоков
-for i in range(1, L):
-    lambdai[i] = ( omega[i] / omega[0] ) * lambdai[0]
+    # Создаем матрицу интенсивностей потоков
+    lambdai = numpy.zeros(L + 1)
+    lambdai[0] = lambda0
 
-# Вывод интенсивностей потоков
-print(lambdai)
+    # Рассчет интенсивностей потоков
+    for i in range(1, L):
+        lambdai[i] = ( omega[i] / omega[0] ) * lambdai[0]
 
-# Коэффициент использования обслуживающих приборов системы (коэффициент загрузки).
-psi = lambdai / mu
+    # Вывод интенсивностей потоков
+    #print(lambdai)
 
-# Рассчет м.о. числа требований в системе
-Mn = psi / ( 1 - psi )
+    # Коэффициент использования обслуживающих приборов системы (коэффициент загрузки).
+    psi = lambdai / mu
+
+    # Рассчет м.о. числа требований в системе
+    Mn.append( psi / ( 1 - psi ) )
+
+    #  Рассчет м.о. длительности пребывания требований в системе
+    Mu.append( 1 / ( mu - lambdai ) )
+
 print(Mn)
 
-#  Рассчет м.о. длительности пребывания требований в системе
-Mu = 1 / ( mu - lambdai )
 print(Mu)
 
+dpi = 80
+fig = plt.figure(dpi = dpi, figsize = (512 / dpi, 384 / dpi) )
+plt.plot(lambdas0, Mn)
+fig.savefig('Mn_lambda0_graph.png')
+
+fig.clear()
+plt.plot(lambdas0, Mu)
+fig.savefig('Mu_lambda0_graph.png')
